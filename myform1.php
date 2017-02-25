232b446dc09ff1316d788e89a8f2a27c36474bf7
eviebail <ebail@seas.upenn.edu>
2017-02-25 13:58:32 -0500

    Update myform1.php

--- myform1.php
+++ myform1.php
@@ -24,6 +24,8 @@ if($_POST['formSubmit'] == "Submit")
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+	<meta content="utf-8" http-equiv="encoding">	
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
