2cde187963da7b5c9830d480b772cec3966e4c31
Evie Bailey <Evie@huntsman-ve565-1002.apn.wlan.upenn.edu>
2017-02-25 17:52:28 -0500

    fixed bugs

--- js/process.js
+++ js/process.js
@@ -1,6 +1,9 @@
-function getValue() {
-  var val = document.getElementById("link").value;
-  window.location.href = 'page2.html';
-  console.log(window.location.pathname);
-  return val;
-}
+var txt = '';
+
+  function getValue() {
+    var val = document.getElementById("link").value;
+    window.location.href = 'page2.html';
+    console.log(window.location.pathname);
+    txt = val;
+  }
+  
