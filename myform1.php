eb9425551e244be66aa501e6e3fb7e35ef3ffd2a
eviebail <ebail@seas.upenn.edu>
2017-02-25 13:47:36 -0500

    Add files via upload

--- myform1.php
+++ myform1.php
@@ -21,30 +21,61 @@ if($_POST['formSubmit'] == "Submit")
 	}
 }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-	<title>My Form</title>
-</head>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Internships, FUNternships</title>
 
-<body>
-	<?php
-		if(!empty($errorMessage))
-		{
-			echo("<p>There was an error with your form:</p>\n");
-			echo("<ul>" . $errorMessage . "</ul>\n");
-		}
-	?>
-	<form action="myform1.php" method="post">
-		<p>
-			What is your favorite movie?<br>
-			<input type="text" name="formMovie" maxlength="50" value="<?=$varMovie;?>" />
-		</p>
-		<p>
-			What is your name?<br>
-			<input type="text" name="formName" maxlength="50" value="<?=$varName;?>" />
-		</p>
-		<input type="submit" name="formSubmit" value="Submit" />
-	</form>
-</body>
+	<!--Bootstrap-->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <link href= "main.css" rel="stylesheet">
+
+	<meta name="description" content="DESCRIBE YOUR WEBSITE">
+	<meta name="keywords" content="KEY, WORDS, HERE">
+	</head>
+	<body>
+		<div class="container bg-1 text-center">
+	  		<h1> Internships, FUNternships </h1>
+	  		<div class="row">
+		  		<div class="row-sm-height">
+		    		<div class="col-sm-4 col-height col-sm-middle text-center" >
+		    			<div class="inside bg-2">
+		    			<br>
+		      			<h4>Tired of searching endlessly for internships? This website
+						makes it easy to read internships related to your interests!
+						Enter the link below, and we'll return keywords from the posting.
+						Type in the box below to get started. </h4>
+						<br>
+						</div>
+		    		</div>
+		    		<div class="col-sm-8 col-height ">
+		    			<div class="inside bg-3">
+		    			<div class="form-group">
+		      				<form action="myform1.php" method="post">
+		  					<br>Enter Link Here: <br><input type="text" name="fname"><br>
+		  					<input type="submit" value="Search!">
+							</form>
+						</div>
+						</div>
+		    		</div>
+	    		</div>
+	    	</div>
+		</div>
+
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    	<!-- Include all compiled plugins (below), or include individual files as needed -->
+    	<script src="js/bootstrap.min.js"></script>
+  	</body>
 </html>
